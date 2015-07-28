63d57100e0e276dca6209275d00f5acb95a92d7b
Acangel <Andres_VP28@hotmail.com>
2015-07-28 16:51:07 -0500

    galeria

---
+++ principal.js
@@ -0,0 +1,23 @@
+function volver() {
+	document.getElementById('imgprincipal').src='./imagenes/1.jpg'
+}
+
+function img1() {
+	document.getElementById('imgprincipal').src='./imagenes/1.jpg';
+}
+
+function img2() {
+	document.getElementById('imgprincipal').src='./imagenes/2.jpg';
+}
+
+function img3() {
+	document.getElementById('imgprincipal').src='./imagenes/3.jpg';
+}
+
+function img4() {
+	document.getElementById('imgprincipal').src='./imagenes/4.jpg';
+}
+
+function img5() {
+	document.getElementById('imgprincipal').src='./imagenes/5.jpg';
+}
